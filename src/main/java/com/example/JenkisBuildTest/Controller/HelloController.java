e53c42b5250a468872b7b5a187dd454e709ff513
Manish Vishwakarma <mv42324@gmail.com>
2023-06-26 19:46:54 +0530

    Examples

--- src/main/java/com/example/JenkisBuildTest/Controller/HelloController.java
+++ src/main/java/com/example/JenkisBuildTest/Controller/HelloController.java
@@ -5,11 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("user")
+@RequestMapping("/api")
 public class HelloController {
 
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello, world!";
+	}
+
+	@GetMapping("/greet")
+	public String greet() {
+		return "Greetings from Spring Boot!";
 	}
 }
